aa985730cd334875177fb589d348e77b6580fbaa
Goran Udosic <gor181@users.noreply.github.com>
2018-04-24 23:00:55 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,9 @@
-# If bash command fails, build should error out
+# If bash command fails, build should fail with an error.
 set -e
 
-##### Install specific package versions with pip #####
+# Install specific package versions with pip
+# NOTE: matplotlib 1.5.3 onward is NOT currently supported. The image comes installed with matplotlib 1.5.1.
 
-# pip3 install pandas==0.22.0
-# pip3 install matplotlib==2.1.2
-# pip3 install scikit-learn==0.19.1
-
-
+echo "about to pip3 install geopandas"
+pip3 install geopandas
+echo 'geopandas version is' $(python3 -c 'import geopandas; print(geopandas.__version__)')
